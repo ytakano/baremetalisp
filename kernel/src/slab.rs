7dba88dfff3ba0eca05be393aa7c780126ec3f1d
Yuuki Takano <ytakanoster@gmail.com>
2020-03-25 11:41:56 +0900

    implemented small slabs

--- kernel/src/slab.rs
+++ kernel/src/slab.rs
@@ -4,43 +4,15 @@ trait Slab {
     fn alloc(&mut self) -> *mut u8;
 }
 
-struct Slab8 {
-    buf: [u64; 8063],
-    next: *mut Slab8,
-    l1_bitmap: [u64; 2],   // l1_bitmap[1] = 0b11 (initial value)
-    l2_bitmap: [u64; 126], // l2_bitmap[125] = 1 (initial value)
-}
-
-impl Slab for Slab8 {
-    fn alloc(&mut self) -> *mut u8 {
-        let idx1 = if self.l1_bitmap[0] == !0 {
-            clz(!self.l1_bitmap[1]) + 64
-        } else {
-            clz(!self.l1_bitmap[0])
-        } as usize;
-
-        let idx2 = clz(!self.l2_bitmap[idx1]) as usize;
-
-        self.l2_bitmap[idx1] |= 1 << (63 - idx2);
-        if self.l2_bitmap[idx1] == !0 {
-            if idx1 >= 64 {
-                self.l1_bitmap[1] |= 1 << (63 - idx1);
-            } else {
-                self.l1_bitmap[0] |= 1 << (63 - idx1);
-            }
-        }
-
-        &mut (self.buf[idx1 * 64 + idx2]) as *mut u64 as *mut u8
-    }
-}
-
 macro_rules! SlabSmall {
-    ($id:ident, $n:expr, $size:expr) => {
+    ($id:ident, $n:expr) => {
         struct $id {
-            buf: [u8; 65536 - 16 - 8 * $n],
-            next: *mut $id,
+            buf: [u8; 65536 - 32 - 8 * $n],
             l1_bitmap: u64,
-            l2_bitmap: [u64; $n]
+            l2_bitmap: [u64; $n],
+            prev: *mut $id,
+            next: *mut $id,
+            size: usize
         }
 
         impl Slab for $id {
@@ -53,55 +25,46 @@ macro_rules! SlabSmall {
                     self.l1_bitmap |= 1 << (63 - idx1);
                 }
 
-                &mut (self.buf[idx1 * $size * 64 + idx2 * $size]) as *mut u8
+                &mut (self.buf[idx1 * self.size * 64 + idx2 * self.size]) as *mut u8
             }
         }
     }
 }
 
 // l1_bitmap = 0 (initial value)
-// l2_bitmap[63] = 0xFFFF FFFF | 1 << 32 (initial value)
-SlabSmall!(Slab16, 64, 16);
+// l2_bitmap[63] = 0xFFFF FFFF | 0b11 << 32 (initial value)
+// size = 16
+SlabSmall!(Slab16, 64);
 
 // l1_bitmap = 0xFFFF FFFF (initial value)
 // l2_bitmap[31] = 0b111111111 (initial value)
-SlabSmall!(Slab32, 32, 32);
+// size = 32
+SlabSmall!(Slab32, 32);
 
 // l1_bitmap = 0xFFFF FFFF FFFF (initial value)
 // l2_bitmap[15] = 0b111 (initial value)
-SlabSmall!(Slab64, 16, 64);
+// size = 64
+SlabSmall!(Slab64, 16);
 
 // l1_bitmap = 0xFFFF FFFF FFFF FF (initial value)
-// l2_bitmap[7] = 0b11 (initial value)
-SlabSmall!(Slab128, 8, 128);
+// l2_bitmap[7] = 0b1 (initial value)
+// size = 128
+SlabSmall!(Slab128, 8);
 
 // l1_bitmap = 0xFFFF FFFF FFFF FFF (initial value)
 // l2_bitmap[3] = 0b1 (initial value)
-SlabSmall!(Slab256, 4, 256);
+// size = 256
+SlabSmall!(Slab256, 4);
 
-struct Slab511 {
-    buf: [u8; 65512],
-    next: *mut Slab511,
-    l1_bitmap: [u64; 2], // 0 (initial value)
-}
+// l1_bitmap = 0x3FFF FFFF FFFF FFFF (initial value)
+// l2_bitmap[1] = 0b1 (initial value)
+// size = 512
+SlabSmall!(Slab512, 2);
 
-impl Slab for Slab511 {
-    fn alloc(&mut self) -> *mut u8 {
-        let idx1 = if self.l1_bitmap[0] == !0 {
-            clz(!self.l1_bitmap[1]) + 64
-        } else {
-            clz(!self.l1_bitmap[0])
-        } as usize;
-
-        if idx1 < 64 {
-            self.l1_bitmap[0] |= 1 << (63 - idx1);
-        } else {
-            self.l1_bitmap[1] |= 1 << (63 - (idx1 - 64));
-        }
-
-         &mut (self.buf[idx1 * 511 * 64]) as *mut u8
-    }
-}
+// l1_bitmap = 0x7FFF FFFF FFFF FFFF (initial value)
+// l2_bitmap[0] = 0b1 (initial value)
+// size = 1024
+SlabSmall!(Slab1024, 1);
 
 macro_rules! SlabLarge {
     ($id:ident, $size:expr) => {
