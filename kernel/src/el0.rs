baadc74e0abf9c3594b2c74900424146579b84ce
Yuuki Takano <ytakanoster@gmail.com>
2020-04-02 18:04:02 +0900

    parsed and generated AST of lisp (partially)

--- kernel/src/el0.rs
+++ kernel/src/el0.rs
@@ -6,7 +6,7 @@ pub fn el0_entry() -> ! {
     // initialize memory allocator
     slab::init();
 
-    parser::parse("(add 1 2)");
+    parser::parse("(if true (add (sub -100 -49) 2) 300)");
 
     let p = 0x400000000 as *mut u64;
     unsafe { *p = 10 };
--- kernel/src/parser.rs
+++ kernel/src/parser.rs
@@ -11,46 +11,59 @@
 
 use crate::driver;
 
-use alloc::string::String;
-use alloc::string::ToString;
+use alloc::string::{String, ToString};
 use alloc::collections::linked_list::LinkedList;
-use alloc::vec::Vec;
 
 #[derive(Debug)]
 pub enum Expr {
-    Num(u64),
+    Num(i64),
     ID(String),
+    Bool(bool),
     List(LinkedList<Expr>),
-    Tuple(Vec<Expr>),
-    Apply(Vec<Expr>),
-    NoExpr
+    Tuple(LinkedList<Expr>),
+    Apply(LinkedList<Expr>)
 }
 
-pub fn parse(code: &str) -> Expr {
-    let (e, _c) = parse_expr(code);
-    let msg = format!("AST: {:?}\n", e);
-    driver::uart::puts(&msg);
-    e
-}
-
-pub fn parse_expr(code: &str) -> (Expr, &str) {
+pub fn parse(code: &str) -> Option<Expr> {
     driver::uart::puts(code);
     driver::uart::puts("\n");
 
+    match parse_expr(code) {
+        Some((e, _c)) => {
+            let msg = format!("AST: {:?}\n", e);
+            driver::uart::puts(&msg);
+            Some(e)
+        }
+        None => {
+            None
+        }
+    }
+}
+
+fn parse_expr(code: &str) -> Option<(Expr, &str)> {
     let c = skip_spaces(code);
     match (*c).chars().nth(0) {
-        Some('(') => { (Expr::NoExpr, code) }
-        Some(_a) => {
-            parse_id(c)
+        Some('(') => {
+            parse_apply(c)
         }
-        _ => { (Expr::NoExpr, code) }
+        Some(a) => {
+            if '0' <= a && a <= '9' || a == '-' {
+                parse_num(c)
+            } else {
+                match parse_id_bool(c) {
+                    None => { None }
+                    ret => { ret }
+                }
+            }
+        }
+        _ => { None }
     }
 }
 
 fn skip_spaces(code: &str) -> &str {
     let mut i = 0;
     for s in code.chars() {
-        if s == ' ' || s == '\r' || s == '\n' || s == '\t' {
+        if is_space(s) {
             i += 1;
             continue;
         } else {
@@ -60,23 +73,117 @@ fn skip_spaces(code: &str) -> &str {
     &code[i..]
 }
 
-fn parse_id(code: &str) -> (Expr, &str) {
+fn parse_id_bool(code: &str) -> Option<(Expr, &str)> {
     let mut i = 0;
     for s in code.chars() {
-        if s == ' ' || s == '\r' || s == '\n' || s == '\t' {
+        if s == ')' || s == '(' || is_space(s) {
             break;
         }
         i += 1;
     }
 
-    (Expr::ID(code[..i].to_string()), &code[i..])
+    if i == 0 {
+        None
+    } else {
+        let c = code[..i].to_string();
+        if c == "true" {
+            Some((Expr::Bool(true), &code[i..]))
+        } else if c == "false" {
+            Some((Expr::Bool(false), &code[i..]))
+        } else {
+            Some((Expr::ID(c), &code[i..]))
+        }
+    }
 }
 
-fn parse_apply(code: &str) -> () {
-    let v: Vec<Expr>;
+fn parse_apply(code: &str) -> Option<(Expr, &str)> {
+    let c = &code[1..]; // skip '('
 
-    let c = &code[1..];
-    let e = parse_expr(c);
+    match parse_exprs(c) {
+        Some((list, c)) => {
+            if c.chars().nth(0) == Some(')') {
+                Some((Expr::Apply(list), &c[1..]))
+            } else {
+                None
+            }
+        }
+        None => {
+            None
+        }
+    }
+}
 
-    driver::uart::puts("(\n");
+fn parse_exprs(code: &str) -> Option<(LinkedList<Expr>, &str)> {
+    let mut exprs = LinkedList::<Expr>::new();
+    let mut c = skip_spaces(code);
+
+    loop {
+        match parse_expr(c) {
+            Some((e, c2)) => {
+                exprs.push_back(e);
+                c = c2;
+            }
+            None => {
+                return None;
+            }
+        }
+
+        c = skip_spaces(c);
+        if c.len() == 0 || c.chars().nth(0) == Some(')') {
+            break;
+        }
+    }
+
+    Some((exprs, c))
+}
+
+fn parse_num(code: &str) -> Option<(Expr, &str)> {
+    let mut i = 0;
+
+    let c = if code.chars().nth(0) == Some('-') {
+        i += 1;
+        &code[1..]
+    } else {
+        code
+    };
+
+    for a in c.chars() {
+        if '0' <= a && a <= '9' {
+            i += 1;
+        } else {
+            break;
+        }
+    }
+
+    let c = &code[i..];
+
+    let fun = || {
+        match code[0..i].parse::<i64>() {
+            Ok(num) => {
+                Some((Expr::Num(num), c))
+            }
+            Err(_msg) => {
+                None
+            }
+        }
+    };
+
+    if c.len() == 0 {
+        return fun();
+    }
+
+    match c.chars().nth(0) {
+        Some(c0) => {
+            if c0 == ')' || c0 == '(' || is_space(c0) {
+                fun()
+            } else {
+                None
+            }
+        }
+        None => { None }
+    }
+}
+
+fn is_space(c: char) -> bool {
+    c == ' ' || c == '\r' || c == '\n' || c == '\t'
 }
