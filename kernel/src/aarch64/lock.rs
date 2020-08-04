253751ddb77a92305551f77f19f9c1f5349a4888
Yuuki Takano <ytakanoster@gmail.com>
2020-08-04 15:28:19 +0900

    added bakerly lock

--- kernel/src/aarch64/lock.rs
+++ kernel/src/aarch64/lock.rs
@@ -1,9 +1,15 @@
+use super::cpu;
+use super::mmu::NUM_CPU;
 use core::intrinsics::volatile_load;
 
 pub struct LockVar {
     var: u64,
 }
 
+/// ```
+/// let var = LockVar::new(); // create lock variable
+/// var.lock();               // acquire lock
+/// ```
 impl LockVar {
     pub const fn new() -> LockVar {
         LockVar { var: 0 }
@@ -26,6 +32,7 @@ impl<'a> SpinLock<'a> {
                     return SpinLock { lock: n };
                 }
             }
+            cpu::wait_event();
         }
     }
 }
@@ -33,6 +40,65 @@ impl<'a> SpinLock<'a> {
 impl<'a> Drop for SpinLock<'a> {
     fn drop(&mut self) {
         *self.lock = 0;
+        cpu::send_event();
+    }
+}
+
+/// ```
+/// let ticket = BakeryTicket::new(); // create lock variable
+/// ticket.lock();                    // acquire lock
+/// ```
+pub struct BakeryTicket {
+    entering: [bool; NUM_CPU as usize],
+    number: [usize; NUM_CPU as usize],
+}
+
+impl BakeryTicket {
+    pub const fn new() -> BakeryTicket {
+        BakeryTicket {
+            entering: [false; NUM_CPU as usize],
+            number: [0; NUM_CPU as usize],
+        }
+    }
+}
+
+pub struct BakeryLock<'a> {
+    lock: &'a mut BakeryTicket,
+}
+
+impl<'a> BakeryLock<'a> {
+    fn new(t: &'a mut BakeryTicket) -> BakeryLock<'a> {
+        let core = cpu::get_affinity_lv0() as usize;
+        loop {
+            t.entering[core] = true;
+            let mut max = 0;
+            for v in &t.number {
+                if max < *v {
+                    max = *v;
+                }
+            }
+            t.number[core] = 1 + max;
+            t.entering[core] = false;
+            cpu::send_event();
+
+            for i in 0..(NUM_CPU as usize) {
+                while t.entering[i] {
+                    cpu::wait_event();
+                }
+
+                while t.number[i] != 0 && (t.number[i], i) < (t.number[core], core) {
+                    cpu::wait_event();
+                }
+            }
+        }
+    }
+}
+
+impl<'a> Drop for BakeryLock<'a> {
+    fn drop(&mut self) {
+        let core = cpu::get_affinity_lv0() as usize;
+        self.lock.number[core] = 0;
+        cpu::send_event();
     }
 }
 
@@ -46,7 +112,6 @@ fn test_and_set(n: &mut u64) -> bool {
              ldaxr {3}, [{0}]
              stlxr {4:w}, {2}, [{0}]
              cbnz  {4:w}, 1b
-             clrex
              and   {1}, {3}, #1",
             in(reg) addr,
             lateout(reg) rd,
