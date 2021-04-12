f25841bbd062e098f4841434cc87d18ac0023261
Yuuki Takano <ytakanoster@gmail.com>
2021-04-13 00:03:52 +0900

    added smc.rs

---
+++ kernel/src/smc.rs
@@ -0,0 +1,28 @@
+#[repr(C)]
+pub struct ThreadSmcArgs {
+    a0: u64,
+    a1: u64,
+    a2: u64,
+    a3: u64,
+    a4: u64,
+    a5: u64,
+    a6: u64,
+    a7: u64,
+}
+
+#[no_mangle]
+pub fn thread_handle_std_smc(
+    _a0: u32,
+    _a1: u32,
+    _a2: u32,
+    _a3: u32,
+    _a4: u32,
+    _a5: u32,
+    _a6: u32,
+    _a7: u32,
+) -> u32 {
+    0
+}
+
+#[no_mangle]
+pub fn thread_handle_fast_smc(_args: &mut ThreadSmcArgs) {}
